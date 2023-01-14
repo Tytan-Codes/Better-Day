856acde121c7582a7c7c1209045f7d7bb58a513a
Tysudo <91703892+tysudo@users.noreply.github.com>
2023-01-14 10:55:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -27,22 +27,26 @@ try:
     ██║░░██║░░░██║░░██║░░░╚═╝██║░░
     ╚█████╔╝██╗╚█████╔╝██╗███████╗
     ░╚════╝░╚═╝░╚════╝░╚═╝╚══════╝""")
-        print(f'{Fore.RED} (1) Search')
-        print(' (0) Exit')
+        print(f'{Fore.RED}(1) Search')
+        print('(0) Exit')
         
-        pick = (int(input('What would you like to do? ')))
+        pick = (int(input(f'{Fore.GREEN}What would you like to do? ')))
         
         if pick == 1:
             search()
         if pick == 0:
             os.system('cls')
             exit()
-            
+        else: 
+            os.system('cls')
+            print(f'{Fore.RED + Style.BRIGHT}Please choose a valid input.')
+            time.sleep(3)
+            os.system('py main.py')
 
 
     def search():
         os.system('cls')
-        print("""
+        print(f"""{Fore.WHITE}
     ░██████╗███████╗░█████╗░██████╗░░█████╗░██╗░░██╗
     ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██║░░██║
     ╚█████╗░█████╗░░███████║██████╔╝██║░░╚═╝███████║
@@ -55,8 +59,8 @@ try:
         print(f'{Fore.RED}(4)Search NewEgg')
         print(f'{Fore.RED}(5)Search Google')
         print(f'{Fore.RED}(6)VSCODE Web Builder')
-        print('(7)Quit')
-        search = int(input(f'{Fore.RED}What do you want to do: '))
+        print('(0)Quit')
+        search = int(input(f'{Fore.GREEN}What do you want to do: '))
         if search == 1:
             var1 = str(input('What would you like to search for. Spaces must be +: '))
             os.system('start chrome https://www.amazon.ca/s?k='+var1+'')
@@ -80,15 +84,21 @@ try:
         if search == 6:
             os.system('start chrome vscode.dev')
             os.system('py main.py')
-        if search == 7:
+        if search == 0:
             os.system('cls')
             sys.exit()
+        else: 
+            os.system('cls')
+            print(f'{Fore.RED + Style.BRIGHT}Please choose a valid input.')
+            time.sleep(3)
+            os.system('py main.py')
+
         
     main()
 
 except KeyboardInterrupt:
     os.system('cls')
-    print(f'{Fore.RED + Style.BRIGHT} If you are trying to end the script please type zero when you get the popup.')
+    print(f'{Fore.RED + Style.BRIGHT}If you are trying to end the script please type zero when you get the popup.')
     time.sleep(3)
     os.system('py main.py')
     
