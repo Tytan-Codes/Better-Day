c037f064fe9dcbc89ad2682c601bee7d6f357645
Tytan <91703892+Tytan-Codes@users.noreply.github.com>
2023-01-18 11:37:51 -0800

    Update install.bat
    Signed-off-by: Tytan <91703892+Tytan-Codes@users.noreply.github.com>

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@ git clone https://github.com/tysudo/better_day-0.0.1.git
 cd better_day-0.0.1
 py -m pip install -r requirements.txt
 winget install sl
-winget install htop
+winget install GNU.nano
 py main.py
