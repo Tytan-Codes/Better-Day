222e088b4ebb2a1717105e8616d9193ea30eef8d
Tytan <tytan@thetytan.com>
2023-03-25 16:46:54 -0400

    eee

--- update.py
+++ update.py
@@ -1,11 +1,12 @@
 import subprocess
 
-# Set the GitHub repository URL and the script file name
+# Set the GitHub repository URL, the branch name, and the script file name
 github_url = "https://github.com/tytan-codes/better-day.git"
+branch_name = "Stable"
 script_name = "main.py"
 
 # Get the latest commit hash from the GitHub repository
-cmd = "git ls-remote " + github_url + " HEAD"
+cmd = f"git ls-remote {github_url} {branch_name}"
 result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 if result.returncode != 0:
     print("Error getting latest commit hash from GitHub.")
@@ -22,11 +23,10 @@ current_commit = result.stdout.decode().strip()
 if current_commit == latest_commit:
     print("You are running the latest version of the script.")
 else:
-    # Update to the latest commit
-    cmd = "git pull"
+    # Update to the latest commit on the specified branch
+    cmd = f"git pull {github_url} {branch_name}"
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if result.returncode != 0:
         print("Error updating script from GitHub.")
         exit()
     print("Script updated successfully.")
-
