b623098cb4d6f293292715f2f792be5dec148056
Tytan <tytan@thetytan.com>
2023-03-25 22:14:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 import subprocess
 
-# Set the GitHub repository URL and the script file name
-github_url = "https://github.com/tytan-codes/better-day.git"
-script_name = "main.py"
+# Set the GitHub repository URL and the branch name
+github_url = "https://github.com/username/repository.git"
+branch_name = "Unstable"
 
-# Get the latest commit hash from the GitHub repository
-cmd = "git ls-remote " + github_url + " HEAD"
+# Get the latest commit hash from the GitHub repository for the specified branch
+cmd = f"git ls-remote {github_url} {branch_name}"
 result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 if result.returncode != 0:
     print("Error getting latest commit hash from GitHub.")
@@ -32,15 +32,16 @@ if result.returncode != 0:
     print("Error getting current commit hash.")
     exit()
 current_commit = result.stdout.decode().strip()
+
 if current_commit == latest_commit:
     print("You are running the latest version of the script.")
 else:
-    os.system('cls' if os.name == 'nt' else 'clear')
+    print("A new version of the script is available on GitHub.")
+    print(f"Latest commit on {branch_name}: {latest_commit}")
+
     
     print(f"{Style.BRIGHT + Fore.RED}You are running an outdated version of the script.")
     print(f"{Style.BRIGHT}A new version of the script is available on GitHub.")
-    print(f"{Style.BRIGHT}Latest commit: " + latest_commit)
-    print(f"{Style.BRIGHT}Current commit: " + current_commit)
     print(f"{Style.BRIGHT + Fore.RED}Please update by running update.py")
     print(f'{Style.BRIGHT + Fore.RED}You updating will make your experience better')
     exit()
