7194d5abd1f0051186531bc7fdd74119d015bdb2
Tytan <tytan@thetytan.com>
2023-05-06 08:38:10 -0700

    Update setup.py
    Signed-off-by: Tytan <tytan@thetytan.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='Better Day',
     version='4.5.6',
-    description='It makes your day better',
+    description='It Makes your day better',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Tytan Codes',
     author_email='spam@thetytan.com',
     url='https://github.com/tytan-codes/better-day',
